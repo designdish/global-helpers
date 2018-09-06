c536e4c276e77099f002324a87e55137d3ac95a2
Bowie <bowie.leidy@gmail.com>
2018-09-06 18:28:51 -0400

    pushing to the correct repository this time.

--- app/src/javascript/helpers/_setCookie.js
+++ app/src/javascript/helpers/_setCookie.js
@@ -16,7 +16,7 @@ var setCookie = function(cName, cValue, cExpires, cPath) {
         "; path=" +
         cPath;
 
-    cCount++;
+    var cCount = (cCount === undefined) ? 1 : cCount++;
 
     console.log('--- \n this cookie is called ' + cName + '\n it expires on ' + cExpires + '\n the path is ' + cPath + '\n the value is ' + cValue + '\n the cookie count is up to ' + cCount + '\n                                                                and that\'s all i have to say about that. \n---');
     return cValue;
