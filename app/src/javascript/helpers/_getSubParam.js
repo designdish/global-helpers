bc4d6aaaaa98f4bfc27bdf594d4b229af0461af2
Bowie <bowie.leidy@gmail.com>
2018-09-06 20:48:11 -0400

    update to the get sub parameter script

--- app/src/javascript/helpers/_getSubParam.js
+++ app/src/javascript/helpers/_getSubParam.js
@@ -1,4 +1,16 @@
  var getSubParameter = function(str, start, end) {
-     var regex = new RegExp('/(?<=' + start + ')(.*)(?=' + end + ')/');
+     var exp;
+     var regex;
+     if (start === '') {
+         exp = "(.*(?=" + end + "))";
+
+     } else if (end === '') {
+         exp = "((?<=" + start + ").*)";
+     } else {
+         exp = "(?<=" + start + ")(.*)(?=" + end + "))";
+     }
+     exp = exp.replace(/"'"/g, '');
+     regex = new RegExp(exp);
+
      return regex.exec(str);
  };
